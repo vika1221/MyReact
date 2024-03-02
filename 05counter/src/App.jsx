d578c8a5684849d565308b50a042faaf0c7daf29
Vikash <vikashranjanofficial@gmail.com>
2024-03-02 21:05:45 +0530

    done

--- 05counter/src/App.jsx
+++ 05counter/src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   const add = () => {
     //console.log(count, Math.random());
     if (count < 20) setCount(count + 1);
+    // follow up interview question
+    // if more than one setCount(count + 1) is called then what will happen -> nothing will happen as it does same work so react fiber will send whole as one
+    // if we use will use setCount((prevCount) => prevCount+1) then it will run bcz 'prevCount' means previous count value which is a state
+    
+    // setCount((prevCount) => prevCount+1);
+    // setCount(prevCount => prevCount+1);
+    // setCount((prevCount) => prevCount+1);
+    // setCount((prevCount) => prevCount+1);
   };
 
   return (
